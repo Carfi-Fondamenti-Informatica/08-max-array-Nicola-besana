832888bc059ed119f9fdc399c1520e8424697808
Nicola-besana <115076634+Nicola-besana@users.noreply.github.com>
2022-12-12 15:23:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,24 @@
 
+//
+// Created by besan on 12/12/2022.
+//
+
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+float max( float m[], int dimensione ){
+
+    int i;
+    float c;
+    c=m[0];
+    for (i=0; i<dimensione; i++){
+
+        if(m[i]>=c){
+            c=m[i];
+        }
+    }
+
+    return c;
+
+}
